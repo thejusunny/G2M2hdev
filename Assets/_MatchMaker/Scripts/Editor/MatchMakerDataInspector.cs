e43fdcbe839ff08b5b7316cbc4d27861d92c2f5f
thejusunny <thejusunny@gmail.com>
2024-04-19 11:14:31 +0400

    feat(Editor-tool): Add Editor tools for building Match games
    - Implement CustomEditorWindow to build match games
    - Add functionality to build MxN matrixes of sprites  and Save it into Assets as SO
    - Inspector for SO to edit the Game configuration
    - Add Multi-edit support for MatchMakerWindow

---
+++ Assets/_MatchMaker/Scripts/Editor/MatchMakerDataInspector.cs
@@ -0,0 +1,20 @@
+﻿using Assets._MatchMaker.Scripts.Data;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets._MatchMaker.Scripts.Editor
+{
+    [CustomEditor(typeof(MatchMakerData))]
+    public class MatchMakerDataInspector : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var data = target as MatchMakerData;
+            if (GUILayout.Button("Edit"))
+            {
+                MatchMakerEditor.Open(data);
+            }
+        }
+    }
+}
---
+++ Assets/_MatchMaker/Scripts/Editor/MatchMakerEditor.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using Assets._MatchMaker.Scripts.Data;
+
+namespace Assets._MatchMaker.Scripts.Editor
+{
+    public class MatchMakerEditor : EditorWindow
+    {
+        private Vector2 _gridSize = new Vector2(3, 3);
+        private Vector2 currentGridSize = new Vector2(3, 3);
+        private Sprite[] sprites;
+        private bool _show = false;
+        private Vector2 tileSize = new Vector2(64, 64);
+        private Vector2 refScreenSize = new Vector2(360, 480);
+        private Vector2 scaledTileSize;
+        private string _savelFileName = "Game1";
+        private string _rootPath = "Assets/_MatchMaker/Data/Games/";
+        private string FullPath => _rootPath + _savelFileName + ".asset";
+
+        private string _buildText = "Build";
+        [MenuItem("Tools/MatchMaker")]
+        public static void Open()
+        {
+            MatchMakerEditor window = CreateInstance<MatchMakerEditor>();
+            window.Show();
+        }
+        private void OnDestroy()
+        {
+            _show = false;
+        }
+        public static void Open(MatchMakerData data)
+        {
+            MatchMakerEditor window = CreateInstance<MatchMakerEditor>();
+            window.Show();
+            window.Init(data);
+        }
+        public void Init(MatchMakerData data)
+        {
+            sprites = data.spriteMatrix;
+            currentGridSize = _gridSize = data._dimensions;
+            scaledTileSize = tileSize * Mathf.Min(position.width / refScreenSize.x, 2f);
+            _show = true;
+            _savelFileName = data.name;
+        }
+        private void OnGUI()
+        {
+            GUILayout.Label("Match Maker Editor Tool");
+            currentGridSize.x = EditorGUILayout.FloatField("X Dimension", currentGridSize.x);
+            currentGridSize.y = EditorGUILayout.FloatField("Y Dimension", currentGridSize.y);
+            GUILayout.Space(10);
+            if (GUILayout.Button(_buildText))
+            {
+                _show = true;
+                _gridSize = currentGridSize;
+                sprites = new Sprite[(int)(_gridSize.x * _gridSize.y)];
+                _buildText = "Rebuild";
+                scaledTileSize = tileSize * Mathf.Min(position.width / refScreenSize.x, 2f);
+            }
+            if (_show)
+            {
+                GUILayout.Space(10);
+                for (int x = 0; x < _gridSize.x; x++)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.FlexibleSpace();
+                    for (int y = 0; y < _gridSize.y; y++)
+                    {
+
+                        //Debug.Log(x * (int)_gridSize.y + y);
+                        sprites[x * (int)_gridSize.y + y] = EditorGUILayout.ObjectField(sprites[x * (int)_gridSize.y + y], typeof(Sprite), false, GUILayout.Width(scaledTileSize.x), GUILayout.Height(scaledTileSize.y)) as Sprite;
+                    }
+                    GUILayout.FlexibleSpace();
+                    EditorGUILayout.EndHorizontal();
+
+                }
+                GUILayout.Space(10);
+                _savelFileName = EditorGUILayout.TextField("FileName", _savelFileName);
+                if (GUILayout.Button("Save"))
+                {
+                    MatchMakerData matchMakerData = CreateInstance<MatchMakerData>();
+                    matchMakerData.spriteMatrix = sprites;
+                    matchMakerData._dimensions = _gridSize;
+                    matchMakerData.name = _savelFileName;
+                    string uniquePath = "";
+                    if (!Directory.Exists(_rootPath))
+                    {
+                        Directory.CreateDirectory(_rootPath);
+                    }
+                    if (File.Exists(FullPath))
+                    {
+                        AssetDatabase.DeleteAsset(FullPath);
+                    }
+                    uniquePath = AssetDatabase.GenerateUniqueAssetPath(FullPath);
+                    AssetDatabase.CreateAsset(matchMakerData, uniquePath);
+                    AssetDatabase.SaveAssets();
+                }
+                GUI.enabled = false;
+                EditorGUILayout.TextField("FullPath", FullPath);
+                GUI.enabled = false;
+            }
+        }
+    }
+}
